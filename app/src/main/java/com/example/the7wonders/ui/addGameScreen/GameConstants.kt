a977730bf45cebee8111c7f5ba21a0f969a6cbea
ozarski <ozarskiw@gmail.com>
2025-07-22 09:48:46 +0200

    new icons for yellow and purple point types

--- app/src/main/java/com/example/the7wonders/ui/addGameScreen/GameConstants.kt
+++ app/src/main/java/com/example/the7wonders/ui/addGameScreen/GameConstants.kt
@@ -9,7 +9,7 @@ enum class PointType(val pointName: String, val color: Color, val icon: Int) {
     Military("Military", PointTypeColors.military, R.drawable.rounded_swords_24),
     Gold("Gold", PointTypeColors.gold, R.drawable.rounded_money_bag_24),
     Blue("Blue card", PointTypeColors.blue, R.drawable.rounded_theater_comedy_24),
-    Yellow("Yellow card", PointTypeColors.yellow, R.drawable.rounded_compare_arrows_24),
+    Yellow("Yellow card", PointTypeColors.yellow, R.drawable.hugeicons_camel),
     Green("Green card", PointTypeColors.green, R.drawable.rounded_architecture_24),
-    Purple("Purple card", PointTypeColors.purple, R.drawable.rounded_playing_cards_24)
+    Purple("Purple card", PointTypeColors.purple, R.drawable.pop_hammer_sledge)
 }
